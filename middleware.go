b766c7863982938577a5980b58036ac64f11d66b
Florian Bauer <florian@fsrv.xyz>
2024-01-14 18:53:06 +0100

    fix: restructure metrics ; add tests for prometheus middleware
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>

--- middleware.go
+++ middleware.go
@@ -1,11 +1,14 @@
 package webbase
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/bonsai-oss/mux"
 	"github.com/getsentry/sentry-go"
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func sentryMiddleware(next http.Handler) http.Handler {
@@ -28,5 +31,33 @@ func sentryMiddleware(next http.Handler) http.Handler {
 			transaction.SetTag("Status-Code", strconv.Itoa(statusCode))
 			transaction.Status = sentry.HTTPtoSpanStatus(statusCode)
 		}
+	})
+}
+
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+
+		timer := prometheus.NewTimer(httpDuration.With(prometheus.Labels{
+			labelPath:         path,
+			labelFunctionName: FunctionName,
+		}))
+		rw := newResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		statusCode := rw.statusCode
+
+		responseStatus.With(prometheus.Labels{
+			labelPath:         path,
+			labelStatus:       strconv.Itoa(statusCode),
+			labelFunctionName: FunctionName,
+		}).Inc()
+		totalRequests.With(prometheus.Labels{
+			labelPath:         path,
+			labelFunctionName: FunctionName,
+		}).Inc()
+
+		log.Printf("%s %s %d %s", r.Method, r.URL.Path, statusCode, timer.ObserveDuration())
 	})
 }
--- middleware_test.go
+++ middleware_test.go
@@ -5,8 +5,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/bonsai-oss/mux"
 	"github.com/getsentry/sentry-go"
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -41,6 +43,64 @@ func TestSentryMiddleware(t *testing.T) {
 			requestId := response.Header.Get("X-Request-ID")
 			_, parseError := uuid.Parse(requestId)
 			assert.NoError(t, parseError)
+		})
+	}
+}
+
+func TestPrometheusMiddleware(t *testing.T) {
+	tests := []struct {
+		name           string
+		method         string
+		url            string
+		expectedStatus int
+		expectedPath   string
+	}{
+		{
+			name:           "GET request",
+			method:         http.MethodGet,
+			url:            "/testpath",
+			expectedStatus: http.StatusOK,
+			expectedPath:   "/testpath",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Create a request
+			req, err := http.NewRequest(tc.method, tc.url, nil)
+			assert.NoError(t, err)
+
+			// Create a ResponseRecorder to record the response
+			rr := httptest.NewRecorder()
+
+			// Create a router and register the middleware
+			r := mux.NewRouter()
+			r.Handle(tc.expectedPath, prometheusMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tc.expectedStatus)
+			})))
+
+			// Serve the HTTP request
+			r.ServeHTTP(rr, req)
+
+			// Check if the status code is as expected
+			assert.Equal(t, tc.expectedStatus, rr.Code)
+
+			foundMetricsCount := map[string]int{
+				"http_requests_total":        0,
+				"http_response_time_seconds": 0,
+				"response_status":            0,
+				"go_info":                    0,
+				"go_gc_duration_seconds":     0,
+			}
+
+			gatheredMetrics, _ := prometheus.DefaultGatherer.Gather()
+			for _, metricFamily := range gatheredMetrics {
+				foundMetricsCount[metricFamily.GetName()]++
+			}
+
+			for metricName, count := range foundMetricsCount {
+				assert.Equal(t, 1, count, "metric %s not found", metricName)
+			}
 		})
 	}
 }
